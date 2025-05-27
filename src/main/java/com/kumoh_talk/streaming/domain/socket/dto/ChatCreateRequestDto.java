bf128e29ade8c6e3198979919df8d68d1896b36c
dswvgw6412 <dswvgw6412@naver.com>
2025-05-27 17:39:05 +0900

    feat: 채팅 메시지 전송 dto 작성

---
+++ src/main/java/com/kumoh_talk/streaming/domain/socket/dto/ChatCreateRequestDto.java
@@ -0,0 +1,18 @@
+package com.kumoh_talk.streaming.domain.socket.dto;
+
+import java.util.List;
+
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import com.kumoh_talk.streaming.global.config.NoStrip;
+
+public record ChatCreateRequestDto(
+		@NotNull
+		@NoStrip
+		@Size(max = 200, message = "채팅 글자 수는 200자 이하여야 합니다.")
+		String content
+) {
+}
