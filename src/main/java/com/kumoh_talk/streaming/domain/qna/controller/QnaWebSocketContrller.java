6701b61f45c90da3c8f21dd999db656dbdde1ef1
dswvgw6412 <dswvgw6412@naver.com>
2025-05-29 23:55:16 +0900

    feat: qna 기능 구현

---
+++ src/main/java/com/kumoh_talk/streaming/domain/qna/controller/QnaWebSocketContrller.java
@@ -0,0 +1,47 @@
+package com.kumoh_talk.streaming.domain.qna.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.kumoh_talk.streaming.domain.qna.dto.QnaCreateRequestDto;
+import com.kumoh_talk.streaming.domain.qna.dto.QnaㅇeleteRequestDto;
+import com.kumoh_talk.streaming.domain.qna.service.QnaWebSocketService;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class QnaWebSocketController {
+		
+		private final SimpMessagingTemplate template;
+		private final QnaWebSocketService qnaWebSocketService;
+
+		@MessageMapping("/streaming/{streamId}/add-qna")
+		public void addQna(
+				@Payload @Valid QnaCreateRequestDto qnaCreateRequestDto,
+				@DestinationVariable Long streamId,
+				StompHeaderAccessor headerAccessor
+		) {
+				template.convertAndSend("/qna/streaming/" + streamId + "/add",
+						qnaCreateRequestDto);
+				log.info("qna 메시지 전송 - {}", qnaCreateRequestDto.content());
+		}
+
+		@MessageMapping("/streaming/{streamId}/delete-qna")
+		public void deleteQna(
+				@Payload @Valid QnaDeleteRequestDto qnaDeleteRequestDto,
+				@DestinationVariable Long streamId,
+				StompHeaderAccessor headerAccessor
+		) {
+				template.convertAndSend("/qna/streaming/" + streamId + "/delete",
+						qnaDeleteRequestDto);
+				log.info("qna 메시지 삭제 - {}", qnaDeleteRequestDto.content());
+		}
+}
---
+++ src/main/java/com/kumoh_talk/streaming/domain/qna/service/QnaWebSocketService.java
@@ -0,0 +1,8 @@
+package com.kumoh_talk.streaming.domain.qna.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QnaWebSocketService {
+	
+}
