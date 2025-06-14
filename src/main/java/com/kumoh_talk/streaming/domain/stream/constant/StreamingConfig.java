af9307930e548cc3022fb7ddc16de84a19f31819
jungbk0808 <jungbk0808@gmail.com>
2025-06-14 23:43:00 +0900

    fix: streamingConfig setter 추가

--- src/main/java/com/kumoh_talk/streaming/domain/stream/constant/StreamingConfig.java
+++ src/main/java/com/kumoh_talk/streaming/domain/stream/constant/StreamingConfig.java
@@ -1,12 +1,14 @@
 package com.kumoh_talk.streaming.domain.stream.constant;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "streaming")
 @Getter
+@Setter
 public class StreamingConfig {
     private String hlsUrlPrefix;
     private String audioApiUrl;
