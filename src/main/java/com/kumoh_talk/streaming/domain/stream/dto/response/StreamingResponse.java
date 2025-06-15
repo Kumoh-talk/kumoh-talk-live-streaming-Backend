c38a014562ea387aedc2c6a1df645976ba128b2f
jungbk0808 <jungbk0808@gmail.com>
2025-06-15 13:23:36 +0900

    feat: 스트리밍 상세 정보 조회 시 title 추가

--- src/main/java/com/kumoh_talk/streaming/domain/stream/dto/response/StreamingResponse.java
+++ src/main/java/com/kumoh_talk/streaming/domain/stream/dto/response/StreamingResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 @Builder
 public record StreamingResponse(
     Long streamId,
+    String title,
     String camUrl,
     String slideUrl
 ) {
--- src/main/java/com/kumoh_talk/streaming/domain/stream/service/StreamingService.java
+++ src/main/java/com/kumoh_talk/streaming/domain/stream/service/StreamingService.java
@@ -385,6 +385,7 @@ public class StreamingService {
 
         return StreamingResponse.builder()
                 .streamId(streamId)
+                .title(streaming.getTitle())
                 .camUrl(streamingConfig.getAudioApiUrl() + streaming.getCamWatchKey() + "/index.m3u8")
                 .slideUrl(streamingConfig.getAudioApiUrl() + streaming.getSlideWatchKey() + "/index.m3u8")
                 .build();
