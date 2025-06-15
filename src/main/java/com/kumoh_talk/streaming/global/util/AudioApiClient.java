a41b49096118e6be65c1e810c0ce3f7bad641913
jungbk0808 <jungbk0808@gmail.com>
2025-06-15 12:44:10 +0900

    feat: audio api 요청 실패 시에도 서버가 계속 동작하도록 변경

--- src/main/java/com/kumoh_talk/streaming/global/util/AudioApiClient.java
+++ src/main/java/com/kumoh_talk/streaming/global/util/AudioApiClient.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,27 +32,35 @@ public class AudioApiClient {
         startPayload.put("hls_url", streamingConfig.getHlsUrlPrefix() + hlsUrl + "/index.m3u8");
         startPayload.put("session_id", sessionId);
 
-        Map startResponse = webClient.post()
-                .uri("/start")
-                .bodyValue(startPayload)
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
+        try {
+            Map startResponse = webClient.post()
+                    .uri("/start")
+                    .bodyValue(startPayload)
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .block();
 
-        log.info("audio api start request, response: {}", startResponse);
+            log.info("audio api start request, response: {}", startResponse);
+        } catch (WebClientResponseException e) {
+            log.error("audio api start error: {}", e.getMessage());
+        }
     }
 
     public void end(String sessionId) {
         Map<String, Object> endPayload = new HashMap<>();
         endPayload.put("session_id", sessionId);
 
-        Map endResponse = webClient.post()
-                .uri("/end")
-                .bodyValue(endPayload)
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
+        try {
+            Map endResponse = webClient.post()
+                    .uri("/end")
+                    .bodyValue(endPayload)
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .block();
 
-        log.info("audio api end request, response: {}", endResponse);
+            log.info("audio api end request, response: {}", endResponse);
+        } catch (WebClientResponseException e) {
+            log.error("audio api end error: {}", e.getMessage());
+        }
     }
 }
